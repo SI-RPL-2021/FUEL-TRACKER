29ff9f71f063de9df4f0950b5e73c060b39ae439
muhammadfadlib <muhammadfadlib@gmail.com>
2021-04-04 19:23:07 +0700

    Spbu Controller ref #15

---
+++ app/Http/Controllers/Admin/SpbuController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Spbu;
+
+class SpbuController extends Controller
+{
+    public function index(){
+        return view('admin.spbu_dt');
+    }
+
+    public function spbus_dt(Request $request){
+        $spbus = Spbu::all();
+        return datatables($spbus)
+        ->editColumn('spbu_iframe', function($db){
+            return '
+                <a class="btn btn-success" href="javascript:detailRoute(\''.$db->spbu_id.'\')">Lihat Rute</a>
+            ';
+        })
+        ->addColumn('action', function($db){
+            return '
+            <div class="d-flex">
+                <a href="javascript:edit(\''.$db->spbu_id.'\')" title="Edit Data" class="btn btn-sm btn-icon bg-success mx-1 w-50"><i class="icon-database-edit2"></i></a>
+                <a href="javascript:remove(\''.$db->spbu_id.'\')" title="Delete Data" class="btn btn-sm btn-icon bg-danger-800 mx-1 w-50"><i class="icon-database-remove"></i></a>
+            </div>
+            ';
+        })
+        ->rawColumns(['action','spbu_iframe'])->toJson();
+    }
+
+    public function spbus_add(Request $request){
+        $spbu = new Spbu();
+        $spbu->spbu_name = $request->spbu_name;
+        $spbu->spbu_iframe = $request->iframe;
+        $spbu->address = $request->address;
+        $spbu->city = $request->city;
+        $spbu->save();
+        return $spbu->toJson();
+    }
+
+    public function spbus_edit(Request $request){
+        return Spbu::where('spbu_id','=',$request->id)->first()->toJson();
+    }
+
+    public function spbus_save(Request $request){
+        $spbu = Spbu::where('spbu_id','=',intval($request->id))->first();
+        $spbu->spbu_name = $request->spbu_name;
+        $spbu->spbu_iframe = $request->iframe;
+        $spbu->address = $request->address;
+        $spbu->city = $request->city;
+        $spbu->save();
+        return $spbu->toJson();
+    }
+
+    public function spbus_delete(Request $request){
+        return Spbu::where('spbu_id','=',$request->id)->first()->delete();
+    }
+
+}
